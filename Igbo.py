13d0194058e1f9eb5819d69f2404907aa5bed022
erytheboss <theuserguy01@gmail.com>
2021-06-02 12:31:06 +0400

    Update Igbo.py

--- Igbo.py
+++ Igbo.py
@@ -1,1 +1,10 @@
-print('This project is new!')
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='.')
+
+@bot.command()
+async def help(ctx):
+    await ctx.send('no u')
+
+bot.run('YOUR BOT TOKEN')
